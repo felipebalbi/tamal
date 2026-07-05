8b253ae7971afc1af0710de38eae1dafab6dad7d
Felipe Balbi <felipe@balbi.sh>
2026-07-05 09:23:37 -0700

    feat(hdl): add DomInput50 (50 MHz PLL-input domain) for the C5G

--- hdl/src/Tamal/Domain.hs
+++ hdl/src/Tamal/Domain.hs
@@ -21,3 +21,13 @@ createDomain
     { vName = "Dom100"
     , vPeriod = hzToPeriod 100_000_000
     }
+
+-- | 50 MHz reference domain for the C5G oscillator (CLOCK_50_B5B, pin R20). It
+-- is the input to the Cyclone V PLL ('Tamal.Board.CycloneV'); the PLL multiplies
+-- it to the design's 100 MHz 'Dom100'. Asynchronous + ActiveHigh (the 'vSystem'
+-- default), which satisfies 'alteraPllSync''s @HasAsynchronousReset@ requirement.
+createDomain
+  vSystem
+    { vName = "DomInput50"
+    , vPeriod = hzToPeriod 50_000_000
+    }
